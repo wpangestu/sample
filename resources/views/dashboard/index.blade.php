28f7800c3ebde003163028033d3280254a358097
Yusup  Efendi <efendiyusup520@gmail.com>
2020-12-01 04:51:32 +0700

    add adminlte

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app_layout')
+@section('content')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Starter Page</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Starter Page</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Wellcome</h5>
+
+                <p class="card-text">
+                    Hello <span class="bold">{{auth()->user()->name}} ({{auth()->user()->email}})</span><br>
+                    Anda memiliki role :
+                        @foreach(auth()->user()->roles as $role)
+                            {{$role->name}}         
+                        @endforeach
+                </p>
+              </div>
+            </div>
+          </div>
+          <!-- /.col-md-6 -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Members; //Load class Members 
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,12 +15,15 @@ use App\Http\Livewire\Members; //Load class Members
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('login');
+});
+Route::get('tes', function () {
+    auth()->user()->assignRole('user');
 });
 
 Route::group(['middleware' => ['auth:sanctum', 'verified']], function() {
     Route::get('/dashboard', function() {
-        return view('dashboard');
+        return view('dashboard/index');
     })->name('dashboard');
 
     Route::get('/member', Members::class)->name('member'); //Tambahkan routing ini
