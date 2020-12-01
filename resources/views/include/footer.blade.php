ce1b592971ec6cf789ada4665ab8dac5ff397a52
Yusup  Efendi <efendiyusup520@gmail.com>
2020-12-01 22:47:08 +0700

    include footer

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2020 <a href="#">Repairman</a>.</strong>
+    <strong>Copyright &copy; 2020 <a href="#">{{ config('app.name', 'Repairman') }}</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
     <b>Version</b> X.X
