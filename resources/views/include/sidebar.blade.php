08d61b8e86120a94a111fb3577e6cc66a7b4b458
Yusup Efendi <efendiyusup520@gmail.com>
2020-12-09 03:34:03 +0700

    fix sidebar

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -90,13 +90,13 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="pages/forms/advanced.html" class="nav-link">
+                <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Chat Pelanggan</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/forms/editors.html" class="nav-link">
+                <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Chat Teknisi</p>
                 </a>
