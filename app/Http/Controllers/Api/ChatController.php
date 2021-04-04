ebca8eccaa9e942fb7eeeccafd043a8891e9b325
Yusup Efendi <efendiyusup520@gmail.com>
2021-04-04 15:22:10 +0700

    update api chat teknisi

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -119,207 +119,240 @@ class ChatController extends Controller
 
     public function get_message_chat(Request $request)
     {
+        try {
+            //code...
 
-        $user_id = auth()->user()->id;
+            $user_id = auth()->user()->id;
 
-        $chatroom = Chatroom::where('user_1',$user_id)->orWhere('user_2',$user_id);
-
-        $page = $request->has('page') ? $request->get('page') : 1;
-        $limit = $request->has('size') ? $request->get('size') : 10;
-        $chatroom_data = $chatroom->limit($limit)->offset(($page - 1) * $limit);
-        $data = $chatroom_data->get();
-        $total = $chatroom_data->count();
-
-        $new_chatroom_data = [];
-        if($total > 0){
-            foreach ($data as $key => $value) {
-                # code...
-                if($value->user_1 === $user_id){
-                    $name = $value->user_2_data->name??"";
-                    $pinned = $value->pinned_user_1;
-                }else{
-                    $name = $value->user_1_data->name??"";
-                    $pinned = $value->pinned_user_2;
+            $chatroom = Chatroom::where('user_1',$user_id)->orWhere('user_2',$user_id);
+    
+            $page = $request->has('page') ? $request->get('page') : 1;
+            $limit = $request->has('size') ? $request->get('size') : 10;
+            $chatroom_data = $chatroom->limit($limit)->offset(($page - 1) * $limit);
+            $data = $chatroom_data->get();
+            $total = $chatroom_data->count();
+    
+            $new_chatroom_data = [];
+            if($total > 0){
+                foreach ($data as $key => $value) {
+                    # code...
+                    if($value->user_1 === $user_id){
+                        $name = $value->user_2_data->name??"";
+                        $pinned = $value->pinned_user_1;
+                    }else{
+                        $name = $value->user_1_data->name??"";
+                        $pinned = $value->pinned_user_2;
+                    }
+        
+                    $chat = Chat::where('chatroom_id',$value->id)->latest()->first();
+                    $unread_message = Chat::where('chatroom_id',$value->id)
+                                    ->where('to',$user_id)
+                                    ->where('read',false)
+                                    ->count();
+                    $chat_data = [
+                        "message" => $chat->message,
+                        "media" => $chat->media,
+                        "from" => $chat->from,
+                        "is_me" => $user_id==$chat->from?true:false,
+                        "created_at" => $chat->created_at
+                    ];
+                    $chatroom_t = [
+                        "id" => $value->id,
+                        "name" => $name,
+                        "unread_count" => $unread_message,
+                        "avatar" => "",
+                        "pinned" => $pinned,
+                        "last_message" => $chat_data
+                    ];
+        
+                    $new_chatroom_data[] = $chatroom_t;
                 }
-    
-                $chat = Chat::where('chatroom_id',$value->id)->latest()->first();
-                $unread_message = Chat::where('chatroom_id',$value->id)
-                                ->where('to',$user_id)
-                                ->where('read',false)
-                                ->count();
-                $chat_data = [
-                    "message" => $chat->message,
-                    "media" => $chat->media,
-                    "from" => $chat->from,
-                    "is_me" => $user_id==$chat->from?true:false,
-                    "created_at" => $chat->created_at
-                ];
-                $chatroom_t = [
-                    "id" => $value->id,
-                    "name" => $name,
-                    "unread_count" => $unread_message,
-                    "avatar" => "",
-                    "pinned" => $pinned,
-                    "last_message" => $chat_data
-                ];
-    
-                $new_chatroom_data[] = $chatroom_t;
             }
+    
+            $response['page'] = $page;
+            $response['size'] = $limit;
+            $response['total'] = $total;
+            $response['data'] = $new_chatroom_data;
+    
+            return response()->json($response);
+
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json(["message"=>"Terjadi kesalahan ".$th->getMessage()],422);
         }
-
-        $response['page'] = $page;
-        $response['size'] = $limit;
-        $response['total'] = $total;
-        $response['data'] = $new_chatroom_data;
-
-        return response()->json($response);
 
     }
     
     public function get_history_message_chat(Request $request)
     {
         // dd('xe');
-        $user_id = auth()->user()->id;
+        try {
+            
+            $user_id = auth()->user()->id;
 
-        $chatroom = Chatroom::where('open',0);
-        // $chatroom->where('user_1',$user_id)->orWhere('user_2',$user_id);
-
-        $page = $request->has('page') ? $request->get('page') : 1;
-        $limit = $request->has('size') ? $request->get('size') : 10;
-        $chatroom_data = $chatroom->limit($limit)->offset(($page - 1) * $limit);
-        $data = $chatroom_data->get();
-        $total = $chatroom_data->count();
-
-        $new_chatroom_data = [];
-        if($total > 0){
-            foreach ($data as $key => $value) {
-                # code...
-                if($value->user_1 === $user_id){
-                    $name = $value->user_2_data->name??"";
-                    $pinned = $value->pinned_user_1;
-                }else{
-                    $name = $value->user_1_data->name??"";
-                    $pinned = $value->pinned_user_2;
+            $chatroom = Chatroom::where('open',0);
+            // $chatroom->where('user_1',$user_id)->orWhere('user_2',$user_id);
+    
+            $page = $request->has('page') ? $request->get('page') : 1;
+            $limit = $request->has('size') ? $request->get('size') : 10;
+            $chatroom_data = $chatroom->limit($limit)->offset(($page - 1) * $limit);
+            $data = $chatroom_data->get();
+            $total = $chatroom_data->count();
+    
+            $new_chatroom_data = [];
+            if($total > 0){
+                foreach ($data as $key => $value) {
+                    # code...
+                    if($value->user_1 === $user_id){
+                        $name = $value->user_2_data->name??"";
+                        $pinned = $value->pinned_user_1;
+                    }else{
+                        $name = $value->user_1_data->name??"";
+                        $pinned = $value->pinned_user_2;
+                    }
+        
+                    $chat = Chat::where('chatroom_id',$value->id)->latest()->first();
+                    $unread_message = Chat::where('chatroom_id',$value->id)
+                                    ->where('to',$user_id)
+                                    ->where('read',false)
+                                    ->count();
+                    $chat_data = [
+                        "message" => $chat->message,
+                        "media" => $chat->media,
+                        "from" => $chat->from,
+                        "is_me" => $user_id==$chat->from?true:false,
+                        "created_at" => $chat->created_at
+                    ];
+                    $chatroom_t = [
+                        "id" => $value->id,
+                        "name" => $name,
+                        "unread_count" => $unread_message,
+                        "avatar" => "",
+                        "pinned" => $pinned,
+                        "last_message" => $chat_data
+                    ];
+        
+                    $new_chatroom_data[] = $chatroom_t;
                 }
-    
-                $chat = Chat::where('chatroom_id',$value->id)->latest()->first();
-                $unread_message = Chat::where('chatroom_id',$value->id)
-                                ->where('to',$user_id)
-                                ->where('read',false)
-                                ->count();
-                $chat_data = [
-                    "message" => $chat->message,
-                    "media" => $chat->media,
-                    "from" => $chat->from,
-                    "is_me" => $user_id==$chat->from?true:false,
-                    "created_at" => $chat->created_at
-                ];
-                $chatroom_t = [
-                    "id" => $value->id,
-                    "name" => $name,
-                    "unread_count" => $unread_message,
-                    "avatar" => "",
-                    "pinned" => $pinned,
-                    "last_message" => $chat_data
-                ];
-    
-                $new_chatroom_data[] = $chatroom_t;
             }
+    
+            $response['page'] = $page;
+            $response['size'] = $limit;
+            $response['total'] = $total;
+            $response['data'] = $new_chatroom_data;
+    
+            return response()->json($response);
+            
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json(["message"=>"Terjadi kesalahan ".$th->getMessage()],422);
         }
 
-        $response['page'] = $page;
-        $response['size'] = $limit;
-        $response['total'] = $total;
-        $response['data'] = $new_chatroom_data;
-
-        return response()->json($response);
 
     }
 
     public function get_message_by_chatroom_id($id,Request $request)
     {
-        $user_id = auth()->user()->id;
+        try {
+            //code...
 
-        $chatroom_id = $id;
+            $user_id = auth()->user()->id;
 
-        $chatroom = Chatroom::find($id);
-
-        $page = $request->has('page') ? $request->get('page') : 1;
-        $limit = $request->has('size') ? $request->get('size') : 10;
+            $chatroom_id = $id;
+    
+            $chatroom = Chatroom::find($id);
+    
+            $page = $request->has('page') ? $request->get('page') : 1;
+            $limit = $request->has('size') ? $request->get('size') : 10;
+            
+            if(!empty($chatroom)){
+                // dd('in');
+                $chat = Chat::where('chatroom_id',$chatroom->id);
+                $chat_data = $chat->limit($limit)->offset(($page - 1) * $limit)->latest();
+    
+                $data = $chat_data->get();
+                $total = $chat_data->count();
+    
+                $new_data = [];
+                foreach ($data as $key => $value) {
+                    # code...
+                    $t_data = [
+                        "id" => $value->id,
+                        "message" => $value->message,
+                        "media" => "",
+                        "from" => $value->from,
+                        "is_me" => $value->from==$user_id?true:false,
+                        "created_at" => $value->created_at
+                    ];
+                    $new_data[] = $t_data;
+                }
+    
+                $response['page'] = $page;
+                $response['size'] = $limit;
+                $response['total'] = $total;
+                $response['data'] = $new_data;
         
-        if(!empty($chatroom)){
-            // dd('in');
-            $chat = Chat::where('chatroom_id',$chatroom->id);
-            $chat_data = $chat->limit($limit)->offset(($page - 1) * $limit)->latest();
-
-            $data = $chat_data->get();
-            $total = $chat_data->count();
-
-            $new_data = [];
-            foreach ($data as $key => $value) {
-                # code...
-                $t_data = [
-                    "id" => $value->id,
-                    "message" => $value->message,
-                    "media" => "",
-                    "from" => $value->from,
-                    "is_me" => $value->from==$user_id?true:false,
-                    "created_at" => $value->created_at
-                ];
-                $new_data[] = $t_data;
+                return response()->json($response);
+                    
             }
 
-            $response['page'] = $page;
-            $response['size'] = $limit;
-            $response['total'] = $total;
-            $response['data'] = $new_data;
-    
-            return response()->json($response);
-                
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json(["message"=>"Terjadi kesalahan ".$th->getMessage()],422);
         }
+
     }
 
     public function get_history_message_by_chatroom_id($id,Request $request)
     {
-        $user_id = auth()->user()->id;
+        try {
+            //code...
 
-        $chatroom_id = $id;
+            $user_id = auth()->user()->id;
 
-        $chatroom = Chatroom::find($id);
+            $chatroom_id = $id;
 
-        $page = $request->has('page') ? $request->get('page') : 1;
-        $limit = $request->has('size') ? $request->get('size') : 10;
+            $chatroom = Chatroom::find($id);
+
+            $page = $request->has('page') ? $request->get('page') : 1;
+            $limit = $request->has('size') ? $request->get('size') : 10;
+            
+            if(!empty($chatroom)){
+                // dd('in');
+                $chat = Chat::where('chatroom_id',$chatroom->id);
+                $chat_data = $chat->limit($limit)->offset(($page - 1) * $limit)->latest();
+
+                $data = $chat_data->get();
+                $total = $chat_data->count();
+
+                $new_data = [];
+                foreach ($data as $key => $value) {
+                    # code...
+                    $t_data = [
+                        "id" => $value->id,
+                        "message" => $value->message,
+                        "media" => "",
+                        "from" => $value->from,
+                        "is_me" => $value->from==$user_id?true:false,
+                        "created_at" => $value->created_at
+                    ];
+                    $new_data[] = $t_data;
+                }
+
+                $response['page'] = $page;
+                $response['size'] = $limit;
+                $response['total'] = $total;
+                $response['data'] = $new_data;
         
-        if(!empty($chatroom)){
-            // dd('in');
-            $chat = Chat::where('chatroom_id',$chatroom->id);
-            $chat_data = $chat->limit($limit)->offset(($page - 1) * $limit)->latest();
-
-            $data = $chat_data->get();
-            $total = $chat_data->count();
-
-            $new_data = [];
-            foreach ($data as $key => $value) {
-                # code...
-                $t_data = [
-                    "id" => $value->id,
-                    "message" => $value->message,
-                    "media" => "",
-                    "from" => $value->from,
-                    "is_me" => $value->from==$user_id?true:false,
-                    "created_at" => $value->created_at
-                ];
-                $new_data[] = $t_data;
+                return response()->json($response);
+                    
             }
 
-            $response['page'] = $page;
-            $response['size'] = $limit;
-            $response['total'] = $total;
-            $response['data'] = $new_data;
-    
-            return response()->json($response);
-                
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json(["message"=>"Terjadi kesalahan ".$th->getMessage()],422);
         }
+        
     }
 
     public function delete_chat(Request $request){
@@ -351,7 +384,7 @@ class ChatController extends Controller
         } catch (\Throwable $th) {
             //throw $th;
             DB::rollback();
-            return response()->json(["message" => "Terjadi kesalahan : ".$th->getMessage()]);
+            return response()->json(["message" => "Terjadi kesalahan : ".$th->getMessage()],422);
         }
     }
 
@@ -384,7 +417,7 @@ class ChatController extends Controller
         } catch (\Throwable $th) {
             //throw $th;
             DB::rollback();
-            return response()->json(["message" => "Terjadi kesalahan : ".$th->getMessage()]);
+            return response()->json(["message" => "Terjadi kesalahan : ".$th->getMessage()],422);
         }
     }
 
@@ -419,7 +452,7 @@ class ChatController extends Controller
 
         } catch (\Throwable $th) {
             //throw $th;
-            return response()->json(["message" => "Terjadi kesalahan : ".$th->getMessage()]);                        
+            return response()->json(["message" => "Terjadi kesalahan : ".$th->getMessage()],422);                        
         }
 
     }
@@ -455,7 +488,7 @@ class ChatController extends Controller
 
         } catch (\Throwable $th) {
             //throw $th;
-            return response()->json(["message" => "Terjadi kesalahan : ".$th->getMessage()]);                        
+            return response()->json(["message" => "Terjadi kesalahan : ".$th->getMessage(),422]);                        
         }
 
     }
