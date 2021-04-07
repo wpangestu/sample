86b3ef94aa1912b0d3f3987c6392c17bc5a2ea15
Yusup Efendi <efendiyusup520@gmail.com>
2021-04-08 06:04:48 +0700

    update api teknisi notifikasi

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -2,44 +2,126 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Order;
+use App\Models\Withdraw;
 use App\Models\Notification;
+use Illuminate\Http\Request;
+use App\Models\ReviewService;
+use App\Http\Controllers\Controller;
 
 class NotificationController extends Controller
 {
     //
     public function index(Request $request)
     {
-        $user_id = auth()->user()->id;
-        $data = Notification::where('user_id',$user_id)->latest();
 
-        $page = $request->has('page') ? $request->get('page') : 1;
-        $limit = $request->has('size') ? $request->get('size') : 10;
-        $notif = $data->limit($limit)->offset(($page - 1) * $limit);
-        $data = $notif->get();
-        $total = $notif->count();
-        $data_new = [];
-        foreach($data as $d => $val){
+        try {
 
-            if(!(is_null($val->service_id))){
+            $user_id = auth()->user()->id;
+            $data = Notification::where('user_id',$user_id)->latest();
+    
+            $page = $request->has('page') ? $request->get('page') : 1;
+            $limit = $request->has('size') ? $request->get('size') : 10;
+            $notif = $data->limit($limit)->offset(($page - 1) * $limit);
+            $data = $notif->get();
+            $total = $notif->count();
+            $data_new = [];
+            foreach($data as $d => $val){
+    
                 $response_data = [
                     "id" => $val->id,
                     "read" => $val->read===0?false:true,
                     "title" => $val->title,
                     "type" => $val->type,
-                    "service_status" => $val->service->status,
+                    "id_data" => $val->id_data,
                     "date" => $val->created_at,
                 ];
+    
+                if($val->type==="service_info"){
+                    
+                    $extra = [
+                        "service_status" => $val->service_status
+                    ];
+                    $response_data = array_merge($response_data, $extra);
+    
+                }elseif($val->type ==="review"){
+                    
+                    $review = ReviewService::find($val->id_data);
+                    if(is_null($review)){
+                        $extra= [
+                            "rating" => null
+                        ];
+                    }else{
+                        $extra= [
+                            "rating" => (int)$review->ratings
+                        ];
+                    }
+                    $response_data = array_merge($response_data, $extra);
+    
+                }elseif($val->type==="wallet"){
+    
+                    $withdraw = Withdraw::find($val->id_data);
+                    if(is_null($withdraw)){
+                        $extra['wallet']=[
+                            "amount" => null,
+                            "message" => "",
+                            "color" => ""
+                        ];
+                    }else{
+                        $extra['wallet']=[
+                            "amount" => (int)$withdraw->amount,
+                            "message" => "Berhasi dikirim ke nomor rekening Kamu.",
+                            "color" => "#27AE60"
+                        ];
+                    }
+                    $response_data = array_merge($response_data, $extra);
+    
+                }elseif($val->type==="order"){
+    
+                    $order = Order::find($val->id_data);
+                    if(is_null($order)){
+                        $extra['order'] = [
+                            "name" => null,
+                            "total_service" => null
+                        ];
+                    }else{
+                        if($order->order_type==="custom"){
+                            $extra['order'] = [
+                                "name" => "Custom Order",
+                                "total_service" => 1
+                            ];
+                        }else{
+                            $count = 0;
+                            foreach ($order->order_detail as $key => $value) {
+                                # code...
+                                $count += $value->qty;
+                            }
+    
+                            $extra['order'] = [
+                                "name" => $order->order_detail[0]->name,
+                                "total_service" => $count
+                            ];
+                        }
+                    }
+                    $response_data = array_merge($response_data, $extra);
+                
+                }
+    
+                $data_new[] = $response_data;
             }
-            $data_new[] = $response_data;
+    
+            $response['page'] = (int)$page;
+            $response['size'] = (int)$limit;
+            $response['total'] = (int)$total;
+            $response['data'] = $data_new;
+    
+            return response()->json($response);        
+
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json(["message" => "Terjadi kesalahan ".$th->getMessage()], 422);
         }
 
-        $response['page'] = (int)$page;
-        $response['size'] = (int)$limit;
-        $response['total'] = (int)$total;
-        $response['data'] = $data_new;
-
-        return response()->json($response);           
+   
     }
 }
---
+++ database/migrations/2021_04_08_050638_update_notification_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateNotificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('notifications', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('id_data')->nullable();
+            $table->string('service_status')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('notifications', function (Blueprint $table) {
+            //
+            $table->dropColumn(['id_data','service_status']);
+        });
+    }
+}
